d991857a1a91f73d4580f3c5ee9c460bea37910b
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-12-12 18:48:18 +0530

    Made iFruit Lockscreen

---
+++ components/notch.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Notch = (props) => {
+  return (
+    <div className='notch absolute top-0 inset-x-0'>
+      <div className='relative h-6 w-40 mx-auto bg-black rounded-b-2xl'></div>
+    </div>
+  )
+}
